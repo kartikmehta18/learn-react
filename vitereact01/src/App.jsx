8788743a2b5c85c99d888255fb309b5b63a50fae
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-05-16 20:08:47 +0530

    react

--- vitereact01/src/App.jsx
+++ vitereact01/src/App.jsx
@@ -3,10 +3,10 @@
 import Chai from "./chai"
 
 function App() {
-
+const username=" kartik mehta";
   return (  //jsx want only 1 element so we take onle element 
 <>
-<h1>Chia-aur-react</h1>
+<h1>Chia-aur-react{username}</h1>
    <Chai />
    <h2>finall react</h2>
 </>
--- vitereact01/src/main.jsx
+++ vitereact01/src/main.jsx
@@ -1,21 +1,26 @@
-// import React from 'react'
+import React from 'react'
 import ReactDOM from 'react-dom/client'
-// import App from './App.jsx'
+import App from './App.jsx'
 
-function MyApp(){
-  return(
-    <div>
-      <h1>custom app</h1>
-    </div>
-  )
-}
-
+// function MyApp(){
+//   return(
+//     <div>
+//       <h1>custom app</h1>
+//     </div>
+//   )
+// }
+const reactElement = React.createElement(
+  'a',
+  {href:'https:/google.com',target:'_blank'},
+  'click me ti visit google',
+)
 const anotherElement = (
   <a href="https:/google.com" target='_blank'>Visit Google</a>
 )
 
 ReactDOM.createRoot(document.getElementById('root')).render(
- 
-    anotherElement
+  // reactElement
+  <App />
+    // anotherElement
  
 )
