7354a18c72615b6e6fde5d9f03f27237bc982eb9
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-06-09 18:14:44 +0530

    reactrouter

--- 06currencyapp/src/hooks/useCurrencyinfo.js
+++ 06currencyapp/src/hooks/useCurrencyinfo.js
@@ -15,34 +15,3 @@ function useCurrencyInfo(currency){
     return data
 }
 export default useCurrencyInfo;
-
-
-// import { useEffect, useState } from "react";
-
-// function useCurrencyInfo(currency) {
-//     const [data, setData] = useState({});
-
-//     useEffect(() => {
-//         const fetchCurrencyData = async () => {
-//             try {
-//                 const response = await fetch(`https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${currency}.json`);
-//                 const result = await response.json();
-//                 setData(result[currency]);
-//             } catch (error) {
-//                 console.error("Error fetching currency data:", error);
-//             }
-//         };
-
-//         if (currency) {
-//             fetchCurrencyData();
-//         }
-//     }, [currency]);
-
-//     useEffect(() => {
-//         console.log(data);
-//     }, [data]);
-
-//     return data;
-// }
-
-// export default useCurrencyInfo;
---
+++ 07reactrouter/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+import './App.css'
+import Header from './components/Header/Header'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     <h1 className=' bg-green-600'>react router</h1>
+    </>
+  )
+}
+
+export default App
