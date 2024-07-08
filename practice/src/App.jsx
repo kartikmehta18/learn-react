a053cbd1dcc468c78f580d5ddca736980892f771
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-08 21:30:01 +0530

    practice web

---
+++ practice/src/App.jsx
@@ -0,0 +1,21 @@
+
+
+import Nav from './Nav';
+import Hero from './Hero';
+import Features from './Features';
+import Plan  from './Plan';
+
+function App() {
+
+  return (
+    <>
+    <Nav />
+   <Hero />
+   <Features />
+   <Plan />
+   
+    </>
+  )
+}
+
+export default App
