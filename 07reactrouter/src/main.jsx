1aeec636e5d111e4e77dbedbbd0923d5319a7dc6
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-11 13:28:42 +0530

    react-rout

--- 07reactrouter/src/main.jsx
+++ 07reactrouter/src/main.jsx
@@ -1,22 +1,34 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
-import { createBrowserRouter, router, RouterProvider } from 'react-router-dom'
-import Root from './components/root'
+import { createBrowserRouter,  RouterProvider } from 'react-router-dom'
+import Root from './components/Root'
 import Home from './components/Home/Home'
+import About from './components/About/About'
+import Contact from './components/Contact/Contact'
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Root/>,
+    element: <Root />,
     children:[
       {
         path:"",
         element:<Home/>
-      }
+      },
+      {
+        path: "about",
+        element:<About />
+      },
+      {
+        path: "contact",
+        element:<Contact />
+      },
     ]
   }
 ])
+
+
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider  router={router}/>
