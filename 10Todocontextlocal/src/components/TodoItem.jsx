b7405ef9a7c24306d58f36ccb7f7a7c9956b2f5a
kartikmehta18 <125860170+kartikmehta18@users.noreply.github.com>
2024-07-18 15:18:05 +0530

    Todocontextlocal

--- 10Todocontextlocal/src/components/TodoItem.jsx
+++ 10Todocontextlocal/src/components/TodoItem.jsx
@@ -1,9 +1,21 @@
+import { useState } from "react";
 import { useTodo } from "../contexts";
 
 
-
 function TodoItem({ todo }) {
-    const {updateTodo, deleteTodo, toggleCompleted} =useTodo
+    const [ isTodoEditable,setIsTodoEditable ] = useState(false)
+    const [todoMsg, setTodoMsg]= useState(todo.todo)
+    const {updateTodo , deleteTodo, toggleComplete } =useTodo()
+
+    const editTodo = () =>{
+        updateTodo(todo.id,{...todo, toto: todoMsg})
+        setIsTodoEditable(false);
+    }
+    const toggleCompleted = () =>{
+        toggleComplete(todo.id)
+    }
+
+
 
     return (
         <div
